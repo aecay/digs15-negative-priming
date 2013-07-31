2651bed45c01e8bb6cc52d4af626c5e51b53932c
Aaron Ecay <aaronecay@gmail.com>
2013-07-31 02:04:45 -0400

    data.R: various changes
    - include new column names
    - exclude non-finites
    - calculate “reverse priming” for the patch

--- scripts/data.R
+++ scripts/data.R
@@ -3,13 +3,19 @@ cleanNegData <- function(path = "queries/coding.cod.ooo") {
 
     df <- df <- read.csv(path, sep = ":", header = FALSE)
     colnames(df) <- c("century", "decade", "unityear", "exclude",
-                      "ne", "not", "clausetype", "id")
+                      "ne", "not", "clausetype", "never.posn", "not.posn",
+                      "finite", "id")
     df$year <- (1000 + 100 * as.numeric(as.character(df$century)) +
                 10 * as.numeric(as.character(df$decade)) +
                 as.numeric(as.character(df$unityear)))
     df$century <- df$decade <- df$unityear <- NULL
 
     df$author <- str_replace(df$id, "-.*", "")
+
+    df$exclude <- factor(ifelse(df$clausetype == "imperative" |
+                                df$finite == "-",
+                                "X",
+                                as.character(df$exclude)))
 
     prev.ne <- rep(NA, times = nrow(df))
     prev.not <- rep(NA, times = nrow(df))
@@ -26,6 +32,20 @@ cleanNegData <- function(path = "queries/coding.cod.ooo") {
     df$prev.ne <- factor(prev.ne)
     df$prev.not <- factor(prev.not)
 
+    ## and we need the next NEG, for calculating the patch
+    next.ne <- rep(NA, times = nrow(df))
+    next.not <- rep(NA, times = nrow(df))
+    for (i in 1:(nrow(df)-1)) {
+        if (df[i+1,]$author == df[i,]$author &&
+            df[i+1,]$exclude != "X" &&
+            df[i,]$exclude != "X") {
+            next.ne[i] <- as.character(df[i+1,]$ne)
+            next.not[i] <- as.character(df[i+1,]$not)
+        }
+    }
+    df$next.ne <- factor(next.ne)
+    df$next.not <- factor(next.not)
+
     df$has.ne <- as.logical(df$ne == "ne")
     df$has.not <- as.logical(df$not %in% c("preverbal","postverbal"))
     df$has.both <- as.logical(df$has.ne & df$has.not)
@@ -34,6 +54,7 @@ cleanNegData <- function(path = "queries/coding.cod.ooo") {
                           ifelse(df$has.ne, "ne",
                                  ifelse(df$has.not, "not", NA)))
 
+    ## calculate derived vars about previous NEG
     df$has.ne.prev <- as.logical(df$prev.ne == "ne")
     df$has.not.prev <- as.logical(
         ifelse(is.na(df$prev.not),
@@ -45,6 +66,19 @@ cleanNegData <- function(path = "queries/coding.cod.ooo") {
                                ifelse(df$has.ne.prev, "ne",
                                       ifelse(df$has.not.prev, "not", NA)))
 
+    ## Same, for next NEG
+    df$has.ne.next <- as.logical(df$next.ne == "ne")
+    df$has.not.next <- as.logical(
+        ifelse(is.na(df$next.not),
+               NA,
+               df$next.not %in% c("nexterbal", "postverbal")))
+    df$has.both.next <- as.logical(df$has.ne.next & df$has.not.next)
+
+    df$next.neg.type <- ifelse(df$has.both.next, "both",
+                               ifelse(df$has.ne.next, "ne",
+                                      ifelse(df$has.not.next, "not", NA)))
+
+    ## A silly attempt to bin dates
     df$datepd <- ifelse(df$year < 1300, 1,
                         ifelse(df$year == 1300, 2, 3))
 
