a0355d0400c87e2a5dd35f351e541be65a20b72f
Aaron Ecay <aaronecay@gmail.com>
2013-07-31 02:46:50 -0400

    graphs.R: more tweaking the three line graph

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -58,13 +58,13 @@ three.lines.graph <- function(neg, write = FALSE) {
                       geom_smooth(aes(weight = total), se = FALSE, linewidth=5) +
                       xlab("Year") +
                       ylab("\\% negative declaratives") +
-                      scale_size_continuous("N", breaks = c(250, 500))
+                      scale_size_continuous("N", breaks = c(750, 1500))
 
     if (write) {
-        tikz("figures/three-lines.tikz", width = 4, height = 1.75)
+        tikz("figures/three-lines.tikz", width = 4, height = 2)
         print(plt + scale_color_brewer("Type", palette = "Set2"))
         dev.off()
-        tikz("figures/three-lines-handout.tikz", width = 4, height = 1.75)
+        tikz("figures/three-lines-handout.tikz", width = 4, height = 2)
         print(plt + scale_color_grey("Type") + theme_minimal())
         dev.off()
     }
